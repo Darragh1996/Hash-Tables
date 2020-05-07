6e49be56e19cb54beea8883abae6d55accd1f676
Darragh Ferry <darraghferry@hotmail.com>
2020-05-07 15:18:09 +0100

    word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,9 +1,27 @@
+import re
+
+
 def word_count(s):
-    # Implement me.
+    count = {}
+    # s = s.translate({ord(c): None for c in ':,.- += /\\|[](}{)*^'})
+    s = re.sub(r'[^a-zA-Z\' ]', '', s)
+    s = s.strip().lower()
+    word_arr = s.split(" ")
+    if len(word_arr) == 1 and word_arr[0] == "":
+        return count
+    for word in word_arr:
+        if word in count:
+            count[word] += 1
+        else:
+            count[word] = 1
+    if "" in count:
+        del count[""]
+    return count
 
 
 if __name__ == "__main__":
-    print(word_count(""))
+    print(word_count("a a\ra\na\ta \t\r\n"))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
