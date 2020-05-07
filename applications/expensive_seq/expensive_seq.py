65324b4039c887b191f9d194ed1a787292a66491
Darragh Ferry <darraghferry@hotmail.com>
2020-05-07 14:29:08 +0100

    expensive_seq

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,22 @@
-def expensive_seq(x, y, z):
-    # Implement me
+cache = {}
+
+
+def exps(x, y, z):
+    search_string = f"num_{x}"
+    if search_string in cache:
+        return cache[search_string]
+    if x <= 0:
+        cache[search_string] = y + z
+        return cache[search_string]
+    if x > 0:
+        cache[search_string] = exps(x-1, y+1, z) + exps(x-2, y+2, z*2) + \
+            exps(x-3, y+3, z*3)
+        return cache[search_string]
+
 
 if __name__ == "__main__":
     for i in range(10):
-        x = expensive_seq(i*2, i*3, i*4)
+        x = exps(i*2, i*3, i*4)
         print(f"{i*2} {i*3} {i*4} = {x}")
 
-    print(expensive_seq(150, 400, 800))
+    print(exps(150, 400, 800))
